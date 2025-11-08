c6ff1a6d4b1b30664368edb71062ccfaf4d35082
RassulUmbetbayev <ras.196727@gmail.com>
2025-11-08 21:21:19 +0500

    Initial commit

--- labs/laba1/laba1/obj/Debug/net8.0/laba1.AssemblyInfo.cs
+++ labs/laba1/laba1/obj/Debug/net8.0/laba1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("laba1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+323032a3eed8959eceaef65cf9134fdf5ce65d44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e849a254d2fcce8431c0d9ae7a58826a9ee4eac")]
 [assembly: System.Reflection.AssemblyProductAttribute("laba1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("laba1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
